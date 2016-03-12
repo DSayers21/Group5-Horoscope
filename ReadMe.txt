435b1f649d6b77a5af09e858a6e3162e8b4d4a5b
James Allington-Kay <lancaster.1@hotmail.co.uk>
2016-03-12 22:51:05 +0000

    Stage 1 - (Start screen) Alpha

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,16 @@
-ReadMe
+***** README *****
+
+Mood:
+
+1: Good
+2: Average
+3: Bad
+
+Age:
+
+A: 0-10
+B: 10-17
+C: 17-23
+D: 23-30
+E: 30+
+
---
+++ js/database.js
@@ -0,0 +1,9 @@
+/* Database option 1 */
+var horoscope = TAFFY([{starsign:"capricorn", mood:"1", age: "a", content:"This is some content about capricorn, mood and their age, woo!"}]);
+/* Database option 2 */
+var horoscope1 = TAFFY([{starsign: "capricorn", content:"This is a horoscope about capricorn"}, 
+                        {mood:"1", content:"This is some more content about the mood"}, 
+                        {age:"a", content:"This is some more more content about the age"} ])
+
+
+
